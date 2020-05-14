ded1b4dcce2f051e93d41db412ce47d74d68aa9b
JamesBradbury <github@jamesbradbury.co.uk>
2020-05-14 21:37:35 +0100

    Add string representation for WeatherForecast

--- wind_forecasts/weather_model.py
+++ wind_forecasts/weather_model.py
@@ -4,12 +4,20 @@ Classes for weather forecast data. Might be turned into Django models at some po
 
 
 class WeatherForecast:
+    """
+    Internal forecast object, independent of source data.
+    """
+
+    def __str__(self):
+        start_str = self.start_datetime.strftime("%H%M %Y%m%d")
+        return f"Forecast at: {start_str}, Wind: {self.expected_wind_speed} from: {self.wind_direction_from}"
 
     def __init__(self, start_datetime, end_datetime, latitude, longitude, expected_wind_speed,
-                 wind_direction_from):
+                 wind_direction_from, pressure):
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
         self.latitude = latitude
         self.longitude = longitude
         self.expected_wind_speed = expected_wind_speed
         self.wind_direction_from = wind_direction_from
+        self.pressure = pressure
