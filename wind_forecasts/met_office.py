592625b4b4c63dcea5b00443eaa9ff7aea36563b
JamesBradbury <github@jamesbradbury.co.uk>
2020-05-14 21:38:33 +0100

    Refactor and calculate gust influence

--- wind_forecasts/met_office.py
+++ wind_forecasts/met_office.py
@@ -1,13 +1,14 @@
 
 import http.client
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+from decimal import Decimal
 
 from local_settings import METOFFICE_CLIENT_ID, METOFFICE_CLIENT_SECRET
 from wind_forecasts.weather_model import WeatherForecast
 
 
-def get_weather_forecast(lat, lon):
+def get_met_office_json(lat, lon):
     conn = http.client.HTTPSConnection("api-metoffice.apiconnect.ibmcloud.com")
 
     headers = {
@@ -25,15 +26,42 @@ def get_weather_forecast(lat, lon):
     return json.loads(data.decode("utf-8"))
 
 
-# Wind speed is in metres per second.
-result = get_weather_forecast(lat=51.27987, lon=-2.77271)
+def calculate_expected_wind_speed(wind_speed, gust_wind_speed):
+    gust_weighting = Decimal("0.1")
+    expected = Decimal(wind_speed) + Decimal(gust_wind_speed) * gust_weighting
+    return expected
 
-forecasts_objects_list = []
-for forecast in result["features"][0]["properties"]["timeSeries"]:
-    end_datetime = datetime.strptime(forecast["time"], "%Y-%m-%dT%H:%MZ")
-    print(end_datetime.strftime("%H=%M %Y-%m-%d"))
-    # new_forecast = WeatherForecast(
-    #     start_datetime=end_datetime)
+
+def get_weather_forecast(lat, lon):
+    forecast_length_hrs = 3
+    forecasts_objects_list = []
+    json_forecast = get_met_office_json(lat=lat, lon=lon)
+
+    for forecast in json_forecast["features"][0]["properties"]["timeSeries"]:
+        end_datetime = datetime.strptime(forecast["time"], "%Y-%m-%dT%H:%MZ")
+        start_datetime = end_datetime - timedelta(hours=forecast_length_hrs)
+        expected_wind_speed = calculate_expected_wind_speed(wind_speed=forecast["windSpeed10m"],
+                                                            gust_wind_speed=forecast["windGustSpeed10m"])
+
+        new_forecast = WeatherForecast(
+            start_datetime=start_datetime,
+            end_datetime=end_datetime,
+            latitude=lat,
+            longitude=lon,
+            expected_wind_speed=Decimal(expected_wind_speed).quantize(Decimal("0.01")),
+            wind_direction_from=int(forecast["windDirectionFrom10m"]),
+            pressure=int(forecast["mslp"]),
+        )
+        forecasts_objects_list.append(new_forecast)
+
+    return forecasts_objects_list
+
+
+result = get_weather_forecast(lat=51.27987, lon=-2.77271)
+print([str(f) for f in result])
+print(result[0])
+print(len(result))
+
 
 # For example:
 #           {
