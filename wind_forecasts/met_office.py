6f388d57b0e07f6647cb767520139d1e97c75ff0
JamesBradbury <github@jamesbradbury.co.uk>
2020-05-18 21:29:35 +0100

    Make met office class derive from WeatherForecast

--- wind_forecasts/met_office.py
+++ wind_forecasts/met_office.py
@@ -8,58 +8,62 @@ from local_settings import METOFFICE_CLIENT_ID, METOFFICE_CLIENT_SECRET
 from wind_forecasts.weather_model import WeatherForecast
 
 
-def get_met_office_json(lat, lon):
-    conn = http.client.HTTPSConnection("api-metoffice.apiconnect.ibmcloud.com")
+class MetOfficeWeatherForecast(WeatherForecast):
 
-    headers = {
-        'x-ibm-client-id': METOFFICE_CLIENT_ID,
-        'x-ibm-client-secret': METOFFICE_CLIENT_SECRET,
-        'accept': "application/json"
-        }
+    def get_met_office_json(self, lat, lon):
+        conn = http.client.HTTPSConnection("api-metoffice.apiconnect.ibmcloud.com")
 
-    conn.request("GET",
-                 f"/metoffice/production/v0/forecasts/point/three-hourly?latitude={lat}&longitude={lon}",
-                 headers=headers)
+        headers = {
+            'x-ibm-client-id': METOFFICE_CLIENT_ID,
+            'x-ibm-client-secret': METOFFICE_CLIENT_SECRET,
+            'accept': "application/json"
+            }
 
-    res = conn.getresponse()
-    data = res.read()
-    return json.loads(data.decode("utf-8"))
+        conn.request("GET",
+                     f"/metoffice/production/v0/forecasts/point/three-hourly?latitude={lat}&longitude={lon}",
+                     headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+        return json.loads(data.decode("utf-8"))
+
+    @staticmethod
+    def calculate_expected_wind_speed(wind_speed, gust_wind_speed):
+        gust_weighting = Decimal("0.1")
+        expected = Decimal(wind_speed) + Decimal(gust_wind_speed) * gust_weighting
+        return expected
+
+    def get_weather_forecast(self, lat, lon):
+        forecast_length_hrs = 3
+        forecasts_objects_list = []
+        json_forecast = self.get_met_office_json(lat=lat, lon=lon)
+
+        for forecast in json_forecast["features"][0]["properties"]["timeSeries"]:
+            end_datetime = datetime.strptime(forecast["time"], "%Y-%m-%dT%H:%MZ")
+            start_datetime = end_datetime - timedelta(hours=forecast_length_hrs)
+            print("###", start_datetime)
+            expected_wind_speed = MetOfficeWeatherForecast.calculate_expected_wind_speed(
+                wind_speed=forecast["windSpeed10m"],
+                gust_wind_speed=forecast["windGustSpeed10m"]
+            )
+
+            new_forecast = MetOfficeWeatherForecast()
+            new_forecast.start_datetime = start_datetime,
+            new_forecast.end_datetime = end_datetime,
+            new_forecast.latitude = lat,
+            new_forecast.longitude = lon,
+            new_forecast.expected_wind_speed = Decimal(expected_wind_speed).quantize(Decimal("0.01")),
+            new_forecast.wind_direction_from = int(forecast["windDirectionFrom10m"]),
+            new_forecast.pressure = int(forecast["mslp"]),
+        
+            forecasts_objects_list.append(new_forecast)
+
+        return forecasts_objects_list
 
 
-def calculate_expected_wind_speed(wind_speed, gust_wind_speed):
-    gust_weighting = Decimal("0.1")
-    expected = Decimal(wind_speed) + Decimal(gust_wind_speed) * gust_weighting
-    return expected
-
-
-def get_weather_forecast(lat, lon):
-    forecast_length_hrs = 3
-    forecasts_objects_list = []
-    json_forecast = get_met_office_json(lat=lat, lon=lon)
-
-    for forecast in json_forecast["features"][0]["properties"]["timeSeries"]:
-        end_datetime = datetime.strptime(forecast["time"], "%Y-%m-%dT%H:%MZ")
-        start_datetime = end_datetime - timedelta(hours=forecast_length_hrs)
-        expected_wind_speed = calculate_expected_wind_speed(wind_speed=forecast["windSpeed10m"],
-                                                            gust_wind_speed=forecast["windGustSpeed10m"])
-
-        new_forecast = WeatherForecast(
-            start_datetime=start_datetime,
-            end_datetime=end_datetime,
-            latitude=lat,
-            longitude=lon,
-            expected_wind_speed=Decimal(expected_wind_speed).quantize(Decimal("0.01")),
-            wind_direction_from=int(forecast["windDirectionFrom10m"]),
-            pressure=int(forecast["mslp"]),
-        )
-        forecasts_objects_list.append(new_forecast)
-
-    return forecasts_objects_list
-
-
-result = get_weather_forecast(lat=51.27987, lon=-2.77271)
+met_office_forecast = MetOfficeWeatherForecast()
+result = met_office_forecast.get_weather_forecast(lat=51.27987, lon=-2.77271)
 print([str(f) for f in result])
-print(result[0])
 print(len(result))
 
 
--- wind_forecasts/weather_model.py
+++ wind_forecasts/weather_model.py
@@ -7,17 +7,19 @@ class WeatherForecast:
     """
     Internal forecast object, independent of source data.
     """
+    start_datetime = None
+    end_datetime = None
+    latitude = None
+    longitude = None
+    expected_wind_speed = None
+    wind_direction_from = None
+    pressure = None
 
     def __str__(self):
+        print("#-#", self.start_datetime)
         start_str = self.start_datetime.strftime("%H%M %Y%m%d")
         return f"Forecast at: {start_str}, Wind: {self.expected_wind_speed} from: {self.wind_direction_from}"
 
-    def __init__(self, start_datetime, end_datetime, latitude, longitude, expected_wind_speed,
-                 wind_direction_from, pressure):
-        self.start_datetime = start_datetime
-        self.end_datetime = end_datetime
-        self.latitude = latitude
-        self.longitude = longitude
-        self.expected_wind_speed = expected_wind_speed
-        self.wind_direction_from = wind_direction_from
-        self.pressure = pressure
+    def __init__(self):
+        pass
+
