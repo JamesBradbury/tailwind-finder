f73175a67cf5d55c382a6f91e273eea0dfa67143
james <github@jamesbradbury.co.uk>
2020-08-24 12:23:19 +0100

    Fix formula for wind angle effect Remove obsolete comments and improve docstring

--- scoring/score_segment_forecasts.py
+++ scoring/score_segment_forecasts.py
@@ -27,7 +27,19 @@ def get_relative_heading(segment_bearing: Union[int, float], wind_direction: Uni
 
 
 def get_wind_angle_effect(relative_heading):
-    return ((sin((relative_heading * 0.0434) / 2.49)) * 25) - 7
+    """
+    Returns values describing the curve of wind effect at different angles (relative_heading).
+    Curve: https://www.desmos.com/calculator/cfdsbfvkuj
+    :param relative_heading: Angle in degrees of wind from front of bike (0=headwind, 180=tailwind)
+    :return: Wind effect factor.
+    """
+    return (
+            (
+                sin((relative_heading * 0.0434 - 176)
+                    / 2.49
+                    )
+             ) * 22
+            ) - 9
 
 
 def score_segment_from_forecast(segment: Dict, forecast: WeatherForecast) -> int:
@@ -35,7 +47,7 @@ def score_segment_from_forecast(segment: Dict, forecast: WeatherForecast) -> int
     Gives a integer score, positive or negative depending on wind speed and direction relative
     to the segment bearing.
     Based loosely on these graphs: https://www.sheldonbrown.com/brandt/wind.html
-    :param segment: Dictionary with the key bearing storing a float
+    :param segment: Dictionary with the key "bearing" storing a float
     :param forecast: Forecast object with wind direction and wind speed
     :return: Score showing how much hindrance/assistance the wind will provide at that forecast time.
     0=no difference.
@@ -44,9 +56,6 @@ def score_segment_from_forecast(segment: Dict, forecast: WeatherForecast) -> int
         return 0
     relative_heading = get_relative_heading(segment_bearing=segment["bearing"],
                                             wind_direction=forecast.wind_direction_from)
-    print("### relative_heading", relative_heading)
-    # Calcated to mirror the effect described in the link above.
-    # ( sin( (0.0434 * x - 176) / 2.49) * 25 ) - 7
     wind_angle_effect = get_wind_angle_effect(relative_heading=relative_heading)
     score = wind_angle_effect * forecast.expected_wind_speed
 
