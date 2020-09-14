36939a215e6b836d7461df3aad1f2c4f9c41da58
james <github@jamesbradbury.co.uk>
2020-09-14 18:42:56 +0100

    Add SegmentsArchive to store segment data locally

--- segments.py
+++ segments.py
@@ -1,7 +1,9 @@
-from typing import Dict
+from csv import DictWriter
+from typing import Dict, Set
 
 from stravalib.model import Segment
 
+from get_strava_client import get_strava_client
 from compassbearing import calculate_initial_compass_bearing
 from local_settings import DEFAULT_MINIMUM_SCORE
 
@@ -14,7 +16,7 @@ class UserSegment:
     threshold_score_to_report = DEFAULT_MINIMUM_SCORE
 
     def __init__(self, segment: Dict):
-        self.strava_id = self.get_clean_segment_id(segment=segment)
+        self.strava_id = segment["strava_id"]
         self.name_str = segment["name"]
         self.start_latlng = segment["start_latlng"]
         self.end_latlng = segment["end_latlng"]
@@ -58,3 +60,49 @@ class UserSegment:
                 except ValueError:
                     raise ValueError(f"Invalid Segment URL {segment_id}. Must end with an integer.")
         return id_int
+
+
+class SegmentsArchive:
+    """Class to download segment data and store it locally."""
+    segments_list = []
+    strava_client = None
+
+    def __init__(self, new_client):
+        self.strava_client = new_client
+
+    def read_segments(self, segments_ids: Set):
+        for seg_id in segments_ids:
+            new_segment = self.strava_client.get_segment(segment_id=seg_id)
+            self.segments_list.append(UserSegment.from_stravalib_segment_object(segment_obj=new_segment))
+
+    def write_segments_to_csv(self):
+        """Writes the segments_dict to a local CSV file for temporary storage."""
+        header = ["strava_id", "name", "start_latlng", "end_latlng", "bearing", "threshold_score_to_report"]
+        with open("full_segment_data.csv", "w") as full_segment_csv_file:
+            csv_writer = DictWriter(full_segment_csv_file, fieldnames=header)
+            csv_writer.writeheader()
+            for segment in self.segments_list:
+                csv_writer.writerow(
+                    rowdict=dict(
+                        strava_id=segment.strava_id,
+                        name=segment.name_str,
+                        start_latlng=segment.start_latlng,
+                        end_latlng=segment.end_latlng,
+                        bearing=segment.bearing,
+                        threshold_score_to_report=segment.threshold_score_to_report
+                    )
+                )
+
+
+# Exploratory code to write segment data locally (less spamming Strava client)
+new_strava_client = get_strava_client(verbose=True)
+new_segments_archive = SegmentsArchive(new_strava_client)
+starred_segs = new_strava_client.get_starred_segments()
+seg_ids = {seg.id for seg in starred_segs}  # if seg.activity_type == "Ride"}
+
+new_segments_archive.read_segments(segments_ids=seg_ids)
+
+athlete_segments = [(s.strava_id, s.name_str) for s in new_segments_archive.segments_list]
+for sid, name_str in athlete_segments:
+    print(f"Segment: {sid} {name_str}")
+new_segments_archive.write_segments_to_csv()
