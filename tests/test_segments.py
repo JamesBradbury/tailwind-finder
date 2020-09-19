728da8a7d68227a27393b7d4d94dd8f9d4f6ee77
james <github@jamesbradbury.co.uk>
2020-09-19 11:26:57 +0100

    Add test for UserSegment

--- tests/test_segments.py
+++ tests/test_segments.py
@@ -1,4 +1,6 @@
 import pytest
+from stravalib.attributes import LatLon
+from stravalib.model import Segment
 
 import segments
 
@@ -22,3 +24,19 @@ def test_get_clean_segment_id_bad_url():
         )
     assert str(exc_info.value) == "Invalid Segment URL https://www.strava.com/segments123456. Must end with an integer."
 
+
+def test_user_segment_from_stravalib_segment_object():
+    seg_obj = Segment(
+        id=12345,
+        name="test_name",
+        start_latlng=LatLon(lat=51.416433, lon=-2.247566),
+        end_latlng=LatLon(lat=51.420233, lon=-2.240333)
+    )
+    new_user_seg = segments.UserSegment.from_stravalib_segment_object(segment_obj=seg_obj)
+    assert new_user_seg.strava_id == 12345
+    assert new_user_seg.name_str == "test_name"
+    assert new_user_seg.start_latlng == LatLon(lat=51.416433, lon=-2.247566)
+    assert new_user_seg.end_latlng == LatLon(lat=51.420233, lon=-2.240333)
+    assert new_user_seg.bearing == 49.88507827763817
+
+# TODO: Test SegmentsArchive
